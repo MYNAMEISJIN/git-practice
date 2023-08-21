2915b6f83b80e00b5388d7b3c47df96f79300cc4
MYNAMEISJIN <89293441+MYNAMEISJIN@users.noreply.github.com>
2023-08-21 21:33:22 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React yeahhh let's go mate
-        </a>
-      </header>
+      this is for github using practicing
+      
+      thank you for viewing my code
     </div>
   );
 }
